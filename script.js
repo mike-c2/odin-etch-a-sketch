e7bccbbb2486afd4429d51a52edbfc8043c7ef6a
Mike Costa <mcosta1@gmail.com>
2023-02-25 22:09:51 -0500

    Change the squareSize to a number
    The unit is still in pixels, but it comes in as a number.  The 'px' is
    added to it on assignment to height and width.

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 function createSquare(squareSize) {
   const square = document.createElement('div');
-  square.classList.add('square');
-  square.style.height = squareSize;
-  square.style.width = squareSize;
+  square.style.height = squareSize + 'px';
+  square.style.width = squareSize + 'px';
   square.style.backgroundColor = 'blue';
   square.style.borderColor = 'red';
   square.style.borderStyle = 'solid';
   square.style.boxSizing = 'border-box';
   
+  console.log(square.offsetHeight);
+  console.log(square.offsetWidth);
+  
   return square;
 }
 
 const sketchContainer = document.getElementById('sketch-container');
-const square = createSquare('100px');
+const square = createSquare(100);
 sketchContainer.appendChild(square);
