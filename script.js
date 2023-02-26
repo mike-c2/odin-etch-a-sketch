2eca7dd9e2bc5c8ab425b4228c2731fb7d0527db
Mike Costa <mcosta1@gmail.com>
2023-02-25 22:35:07 -0500

    Add createGrid function
    The createGrid will take in 4 parameters:
    
      gridHeight
      gridWidth
      numberOfRows
      numberOfColumns
    
    It will then create the appropriate number and sized squares to fill up
    the grid.
    
    Also split the createSquare parameters into height and width.

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
-function createSquare(squareSize) {
+function createSquare(squareHeight, squareWidth) {
   const square = document.createElement('div');
-  square.style.height = squareSize + 'px';
-  square.style.width = squareSize + 'px';
+  square.style.height = squareHeight + 'px';
+  square.style.width = squareWidth + 'px';
   square.style.backgroundColor = 'blue';
   square.style.borderColor = 'red';
   square.style.borderStyle = 'solid';
   square.style.boxSizing = 'border-box';
   
-  console.log(square.offsetHeight);
-  console.log(square.offsetWidth);
-  
   return square;
 }
 
+function createGrid(gridHeight, gridWidth, numberOfRows, numberOfColumns) {
+  const grid = document.createElement('div');
+  grid.style.height = gridHeight + 'px';
+  grid.style.width = gridWidth + 'px';
+  grid.style.boxSizing = 'border-box';
+  grid.style.display = 'grid';
+  
+  const squareHeight = Math.floor(gridHeight / numberOfRows);
+  const squareWidth = Math.floor(gridWidth / numberOfColumns);
+  
+  grid.style.gridTemplateColumns = `repeat(${numberOfColumns}, ${squareWidth + 'px'})`;
+  
+  for(let i = 0; i < numberOfRows * numberOfColumns; i++) {
+    grid.appendChild(createSquare(squareHeight, squareWidth));
+  }
+  
+  return grid;
+}
+
+
+
 const sketchContainer = document.getElementById('sketch-container');
-const square = createSquare(100);
-sketchContainer.appendChild(square);
+const grid = createGrid(500, 500, 10, 10);
+sketchContainer.appendChild(grid);
