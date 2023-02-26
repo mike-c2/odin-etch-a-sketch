c33a5651335db532e0f02febf80e588911adc566
Mike Costa <mcosta1@gmail.com>
2023-02-26 16:28:54 -0500

    Re-align code and remove some extra white-space

--- script.js
+++ script.js
@@ -5,21 +5,21 @@ function createSquare(squareHeight, squareWidth) {
   square.style.backgroundColor = '#000000';
   square.style.opacity = '0';
   square.style.boxSizing = 'border-box';
-  
+
   square.addEventListener('mouseover', darkenSquare);
-  
+
   return square;
 }
 
 function darkenSquare() {
   let opacity = Number(this.style.opacity);
-  
+
   if(opacity < 1) {
     opacity += 0.1;
   }
-  
+
   this.style.opacity = opacity.toString();
-  
+
   if(opacity >= 1) {
     this.removeEventListener('mouseover', darkenSquare);
   }
@@ -32,16 +32,16 @@ function createGrid(gridHeight, gridWidth, numberOfRows, numberOfColumns) {
   grid.style.backgroundColor = '#ffffff';
   grid.style.boxSizing = 'border-box';
   grid.style.display = 'grid';
-  
+
   const squareHeight = Math.floor(gridHeight / numberOfRows);
   const squareWidth = Math.floor(gridWidth / numberOfColumns);
-  
+
   grid.style.gridTemplateColumns = `repeat(${numberOfColumns}, ${squareWidth + 'px'})`;
-  
+
   for(let i = 0; i < numberOfRows * numberOfColumns; i++) {
     grid.appendChild(createSquare(squareHeight, squareWidth));
   }
-  
+
   return grid;
 }
 
